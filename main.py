aa4bdaff35bdb6ebc69fcd64fa299412d2027847
yuvraj tyagi <148048444+yash9o1@users.noreply.github.com>
2023-12-03 00:57:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,397 @@
+
+Q1 = """Q1. Who developed Python Programming Language?
+a) Wick van Rossum
+b) Rasmus Lerdorf
+c) Guido van Rossum
+d) Niene Stom"""
+
+Q2 = """\nQ2. Which type of Programming does Python support?
+a) object-oriented programming
+b) structured programming
+c) functional programming
+d) all of the mentioned"""
+
+Q3 = """\nQ3. Is Python case sensitive when dealing with identifiers?
+a) no
+b) yes
+c) machine dependent
+d) none of the mentioned"""
+
+Q4 = """\nQ4. Which of the following is the correct extension of the Python file?
+a) .python
+b) .pl
+c) .py
+d) .p"""
+
+Q5 = """\nQ5. Is Python code compiled or interpreted?
+a) Python code is both compiled and interpreted
+b) Python code is neither compiled nor interpreted
+c) Python code is only compiled
+d) Python code is only interpreted"""
+
+Q6 = """\nQ6. All keywords in Python are in _________
+a) Capitalized
+b) lower case
+c) UPPER CASE
+d) None of the mentioned"""
+
+Q7  = """\nQ7. Which of the following is used to define a block of code in Python language?
+a) Indentation
+b) Key
+c) Brackets
+d) All of the mentioned"""
+
+Q8 = """\nQ8. Which keyword is used for function in Python language?
+a) Function
+b) def
+c) Fun
+d) Define"""
+
+Q9 = """\nQ9. Which of the following character is used to give single-line comments in Python?
+a) //
+b) #
+c) !
+d) /*"""
+
+Q10 = """\nQ10. Which of the following functions can help us to find the version of python that we are currently working on?
+a) sys.version(1)
+b) sys.version(0)
+c) sys.version()
+d) sys.version"""
+
+Q11 = """\nQ11. Python supports the creation of anonymous functions at runtime, using a construct called __________
+a) pi
+b) anonymous
+c) lambda
+d) none of the mentioned"""
+
+Q12 = """\nQ12. What is the order of precedence in python?
+a) Exponential, Parentheses, Multiplication, Division, Addition, Subtraction
+b) Exponential, Parentheses, Division, Multiplication, Addition, Subtraction
+c) Parentheses, Exponential, Multiplication, Division, Subtraction, Addition
+d) Parentheses, Exponential, Multiplication, Division, Addition, Subtraction"""
+
+Q13 = """\nQ13. What does pip stand for python?
+a) Pip Installs Python
+b) Pip Installs Packages
+c) Preferred Installer Program
+d) All of the mentioned"""
+
+Q14 = """\nQ14. Which of the following is true for variable names in Python?
+a) underscore and ampersand are the only two special characters allowed
+b) unlimited length
+c) all private members must have leading and trailing underscores
+d) none of the mentioned"""
+
+Q15 = """\nQ15. Which of the following is the truncation division operator in Python?
+a) |
+b) //
+c) /
+d) %"""
+
+Q16 = """\nQ16. Which of the following is the use of id() function in python?
+a) Every object doesn’t have a unique id
+b) Id returns the identity of the object
+c) All of the mentioned
+d) None of the mentioned"""
+
+Q17 = """\nQ17. Which of the following is not a core data type in Python programming?
+a) Tuples
+b) Lists
+c) Class
+d) Dictionary"""
+
+Q18 = """\nQ18. Which of these is the definition for packages in Python?
+a) A set of main modules
+b) A folder of python modules
+c) A number of files containing Python definitions and statements
+d) A set of programs making use of Python modules"""
+
+Q19 = """\nQ19. What is the order of namespaces in which Python looks for an identifier?
+a) Python first searches the built-in namespace, then the global namespace and finally the local namespace
+b) Python first searches the built-in namespace, then the local namespace and finally the global namespace
+c) Python first searches the local namespace, then the global namespace and finally the built-in namespace
+d) Python first searches the global namespace, then the local namespace and finally the built-in namespace"""
+
+Q20 = """\nQ20. Which one of the following is not a keyword in Python language?
+a) pass
+b) eval
+c) assert
+d) nonlocal"""
+
+Q21 = """\nQ21. Which module in the python standard library parses options received from the command line?
+a) getarg
+b) getopt
+c) main
+d) os"""
+
+Q22 = """\nQ22. What arithmetic operators cannot be used with strings in Python?
+a) *
+b) –
+c) +
+d) All of the mentioned"""
+
+Q23 = """\nQ23. Which of the following statements is used to create an empty set in Python?
+a) ( )
+b) [ ]
+c) { }
+d) set()"""
+
+Q24 = """\nQ24. To add a new element to a list we use which Python command?
+a) list1.addEnd(5)
+b) list1.addLast(5)
+c) list1.append(5)
+d) list1.add(5)"""
+
+Q25 = """\nQ25. Which one of the following is the use of function in python?
+a) Functions don’t provide better modularity for your application
+b) you can’t also create your own functions
+c) Functions are reusable pieces of programs
+d) All of the mentioned"""
+
+Q26 = """\nQ26. What is the maximum possible length of an identifier in Python?
+a) 79 characters
+b) 31 characters
+c) 63 characters
+d) none of the mentioned"""
+
+Q27 = """\nQ27. What are the two main types of functions in Python?
+a) System function
+b) Custom function
+c) Built-in function & User defined function
+d) User function"""
+
+Q28 = """\nQ28. Which of the following is a Python tuple?
+a) {1, 2, 3}
+b) {}
+c) [1, 2, 3]
+d) (1, 2, 3)"""
+
+Q29 = """\nQ29. Which of the following is a feature of Python DocString?
+a) In Python all functions should have a docstring
+b) Docstrings can be accessed by the __doc__ attribute on objects
+c) It provides a convenient way of associating documentation with Python modules, functions, classes, and methods
+d) All of the mentioned"""
+
+Q30 = """\nQ30. What is output of print(math.pow(3, 2))?
+a) 9.0
+b) None
+c) 9
+d) None of the mentioned"""
+
+Q31 = """\nQ31. Which of the following is the use of id() function in python?
+a) Every object in Python doesn’t have a unique id
+b) In Python Id function returns the identity of the object
+c) None of the mentioned
+d) All of the mentioned"""
+
+Q32 = """\nQ32. The process of pickling in Python includes ____________
+a) conversion of a Python object hierarchy into byte stream
+b) conversion of a datatable into a list
+c) conversion of a byte stream into Python object hierarchy
+d) conversion of a list into a datatable"""
+
+
+Q33 = """\nQ33. The value of the expressions 4/(3*(2-1)) and 4/3*(2-1) is the same.
+a) True
+b) False"""
+
+Q34 = """\nQ34. Who invented Java Programming?
+a) Guido van Rossum
+b) James Gosling
+c) Dennis Ritchie
+d) Bjarne Stroustrup"""
+
+Q35 = """\nQ35. Which statement is true about Java?
+a) Java is a sequence-dependent programming language
+b) Java is a code dependent programming language
+c) Java is a platform-dependent programming language
+d) Java is a platform-independent programming language"""
+
+Q36 = """\nQ36. Which component is used to compile, debug and execute the java programs?
+a) JRE
+b) JIT
+c) JDK
+d) JVM"""
+
+Q37 = """\nQ37. Which one of the following is not a Java feature?
+a) Object-oriented
+b) Use of pointers
+c) Portable
+d) Dynamic and Extensible"""
+
+Q38 = """\nQ38. Which of these cannot be used for a variable name in Java?
+a) identifier & keyword
+b) identifier
+c) keyword
+d) none of the mentioned"""
+
+Q39 = """\nQ39. What is the extension of java code files?
+a) .js
+b) .txt
+c) .class
+d) .java"""
+
+Q40 = """\nQ40. Which environment variable is used to set the java path?
+a) MAVEN_Path
+b) JavaPATH
+c) JAVA
+d) JAVA_HOME"""
+
+Q41 = """\nQ41. Which of the following is not an OOPS concept in Java?
+a) Polymorphism
+b) Inheritance
+c) Compilation
+d) Encapsulation"""
+
+Q42 = """\nQ42. What is not the use of “this” keyword in Java?
+a) Referring to the instance variable when a local variable has the same name
+b) Passing itself to the method of the same class
+c) Passing itself to another method
+d) Calling another constructor in constructor chaining"""
+
+Q43 = """\nQ43. Which of the following is a type of polymorphism in Java Programming?
+a) Multiple polymorphism
+b) Compile time polymorphism
+c) Multilevel polymorphism
+d) Execution time polymorphism"""
+
+Q44 = """\nQ44. What is Truncation in Java?
+a) Floating-point value assigned to a Floating type
+b) Floating-point value assigned to an integer type
+c) Integer value assigned to floating type
+d) Integer value assigned to floating type"""
+
+Q45 = """\nQ45. What is the extension of compiled java classes?
+a) .txt
+b) .js
+c) .class
+d) .java"""
+
+Q46 = """\nQ46. Which exception is thrown when java is out of memory?
+a) MemoryError
+b) OutOfMemoryError
+c) MemoryOutOfBoundsException
+d) MemoryFullException"""
+
+Q47 = """\nQ47. Which of these are selection statements in Java?
+a) break
+b) continue
+c) for()
+d) if()"""
+
+Q48 = """\nQ48. Which of these keywords is used to define interfaces in Java?
+a) intf
+b) Intf
+c) interface
+d) Interface"""
+
+Q49 = """\n49. Which of the following is a superclass of every class in Java?
+a) ArrayList
+b) Abstract class
+c) Object class
+d) String"""
+
+Q50 = """\nQ50. Which of the below is not a Java Profiler?
+a) JProfiler
+b) Eclipse Profiler
+c) JVM
+d) JConsole"""
+
+Q51 = """\nQ51. Which of these packages contains the exception Stack Overflow in Java?
+a) java.io
+b) java.system
+c) java.lang
+d) java.util"""
+
+Q52 = """\nQ52. Which of these statements is incorrect about Thread?
+a) start() method is used to begin execution of the thread
+b) run() method is used to begin execution of a thread before start() method in special cases
+c) A thread can be formed by implementing Runnable interface only
+d) A thread can be formed by a class that extends Thread class"""
+
+Q53 = """\nQ53. Which of these keywords are used for the block to be examined for exceptions?
+a) check
+b) throw
+c) catch
+d) try"""
+
+Q54 = """\nQ54. Which one of the following is not an access modifier?
+a) Protected
+b) Void
+c) Public
+d) Private"""
+
+Q55 = """\nQ55. What is the numerical range of a char data type in Java?
+a) 0 to 256
+b) -128 to 127
+c) 0 to 65535
+d) 0 to 32767"""
+
+Q56 = """\nQ56. Which class provides system independent server side implementation?
+a) Server
+b) ServerReader
+c) Socket
+d) ServerSocket"""
+
+Q57 = """\nQ57. Which of the following is true about servlets?
+a) Servlets can use the full functionality of the Java class libraries
+b) Servlets execute within the address space of web server, platform independent and uses the functionality of java class libraries
+c) Servlets execute within the address space of web server
+d) Servlets are platform-independent because they are written in java"""
+
+
+set_1 = {Q1:'c',Q2:'d',Q3:'b',Q4:'c',Q5:'a',Q6:'d',Q7:'a',Q8:'b',Q9:'b',Q10:'d',Q11:'c',Q12:'d',Q13:'c',Q14:'b',Q15:'b',Q16:'b',Q17:'c',Q18:'b',Q19:'c',Q20:'b'}
+set_2 = {Q21:'b',Q22:'b',Q23:'d',Q24:'c',Q25:'c',Q26:'d',Q27:'c',Q28:'d',Q29:'d',Q30:'a',Q31:'b',Q32:'a',Q33:'a',Q34:'b',Q35:'d',Q36:'c',Q37:'b',Q38:'c',Q39:'d',Q40:'d'}
+set_3 = {Q41:'c',Q42:'b',Q43:'b',Q44:'b',Q45:'c',Q46:'b',Q47:'d',Q48:'c',Q49:'c',Q50:'c',Q51:'c',Q52:'b',Q53:'d',Q54:'b',Q55:'c',Q56:'d',Q57:'b'}
+
+x = input("Select any set (1/2/3):- ")
+points = 0
+if x == '1':
+    for i in set_1:
+        print(i)
+        ans = input("Choose the correct option(a,b,c,d) or 1 to skip question and 0 to quit:- ")
+        if ans == set_1[i]:
+            print("\nyour answer is correct, you got 1 point")
+            points = points + 1
+        elif ans == ('0'):
+            break
+        elif ans == ('1'):
+            continue
+        else:
+            print("\nyour answer is wrong, you lost 1 point")
+
+elif x == '2':
+    for i in set_2:
+        print(i)
+        ans = input("Choose the correct option(a,b,c,d) or 1 to skip question and 0 to quit:- ")
+        if ans == set_2[i]:
+            print("\nyour answer is correct, you got 1 point")
+            points = points + 1
+        elif ans == ('0'):
+            break
+        elif ans == ('1'):
+            continue
+        else:
+            print("\nyour answer is wrong, you lost 1 point")
+elif x == '3':
+    for i in set_3:
+        print(i)
+        ans = input("Choose the correct option(a,b,c,d) or 1 to skip question and 0 to quit:- ")
+        if ans == set_3[i]:
+            print("\nyour answer is correct, you got 1 point")
+            points = points + 1
+        elif ans == ('0'):
+            break
+        elif ans == ('1'):
+            continue
+        else:
+            print("\nyour answer is wrong, you lost 1 point")
+else:
+    print("Invalid input")
+print("your final score is ",points)
+
+
+
+
+
